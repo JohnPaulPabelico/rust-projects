622ebe0b0a7caa461eb4484b298848760f612782
JohnPaulPabelico <pabelicojohnpaul@gmail.com>
2025-05-04 19:26:33 +0800

    update

---
+++ variables/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    let x = 5;
+    println!("The value of x is: {}", x);
+
+    let x = "six";
+    println!("The value of x is: {}", x);
+
+    const COUNT: u32 = 100_000;
+
+    println!("The value of COUNT is: {}", COUNT);
+
+}
